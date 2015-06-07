b3a74b67a5c58c2e7c6175956d12c1a282d337b4
litichevskiy <kunin25@ukr.net>
2015-06-07 22:21:10 +0300

    new Snake

--- snake.js
+++ snake.js
@@ -1,39 +1,96 @@
 
-(function () { 
+
+(function ( ) {
+
+    var createField = {
+
+        xx : 31,
+        yy : 31,
+        f  : this.f,
+        getul : function () {
+            var elem = document.querySelector('[data-snake="field"]');
+            return elem;
+        },
+
+        field : function ( ul,f,s ) {
+            this.f = [];
+
+            for ( var x = 0; x < createField.xx; x++ ) {
+
+                var li = document.createElement('li');
+                 var string = [];
+                this.f.push(string);
+                ul.appendChild(li);
+
+            for ( var y = 0; y < createField.yy; y++ ) {
+
+                var div = document.createElement('div');
+                 string.push(div);
+                li.appendChild(div);
+        
+            }
+            }
+            
+        },
+
+        getarray : function () {
+            return this.f
+        }
+    };
+
+
+    function game_over(){
+        alert( 'GAME OVER' );
+        
+    };
+
+
+    function food( a, b, c  ) {
+
+        var removed = a;
+        removed.classList.remove('food');
+        var f1 = b;
+        var s = Math.floor(Math.random() * f1.length ); 
+        var s1 = Math.floor(Math.random() * f1.length );
+        var snake_body = c;
+        snake_body.push({x:0,y:0});
+        f1[s][s1].classList.add('food');
+       
+   
+    };
+
 
     var getCoordHead = {
-    'up'    : function ( c ) { return { x : c.x    , y : c.y - 1 } },
-    'down'  : function ( c ) { return { x : c.x    , y : c.y + 1 } },
-    'left'  : function ( c ) { return { x : c.x - 1, y : c.y     } },
-    'right' : function ( c ) { return { x : c.x + 1, y : c.y     } },
-};
+        'up'    : function ( c ) { return { x : c.x    , y : c.y - 1 } },
+        'down'  : function ( c ) { return { x : c.x    , y : c.y + 1 } },
+        'left'  : function ( c ) { return { x : c.x - 1, y : c.y     } },
+        'right' : function ( c ) { return { x : c.x + 1, y : c.y     } },
+    };
 
-function Snake ( o ) {
-    this.body = o.body; // head --> 0
-    this.direction = 'right'; // down, left, up
-}
+    function Snake ( o ) {
+        this.body = o.body; // head --> 0
+        this.direction = 'right'; // down, left, up
+    };
 
-Snake.prototype.move = function () {
-    var snake = this;
+
+
+    Snake.prototype.move = function () {
+        var snake = this;
     
-    this.body.unshift(
-        getCoordHead[this.direction]( this.body[0] )
-    );
+        this.body.unshift(
+            getCoordHead[this.direction]( this.body[0] )
+        );
     
-    var removed = this.body.pop();
+        var removed = this.body.pop();
     
-    return {
-        added   : snake.body[0],
-        removed : removed
-    }
-};
- 
+        return {
+            added   : snake.body[0],
+            removed : removed
+        }
+    };
 
-var s = 4;
-var s1 = 6;
-
-
-(function(){
+   (function(createField){
+    var e = createField;
     var snake = new Snake({
     body : [
         { x :  4, y : 15 },
@@ -45,65 +102,75 @@ var s1 = 6;
 });
     
 
-    var a = document.body;
+var a = document.body;
 
 a.onkeydown = function (event) {
-    i = event.keyCode; 
-    return i;
-}
+  
+    coordHead(event.keyCode);
+  
+};
 
+var coordHead = function () {
 
-var i=0,
-    proc = setInterval(function func(){
+    var o = {
 
-f1[s][s1].classList.add('food');
-      
+        37 : snake.direction = 'left',
+        38 : snake.direction = 'up',
+        39 :  snake.direction = 'right',
+        40 : snake.direction = 'down',
+    };
+
+    return (function (i) {
         
-      
-        if ( i === 40 ) { snake.direction = 'down'  };
-        if ( i === 39 ) { snake.direction = 'right' };
-        if ( i === 37 ) { snake.direction = 'left'  };
-        if ( i === 38 ) { snake.direction = 'up'    };
+        snake.direction =  o[i];
+       
+    })(event.keyCode);
+};
+
+
+
+    proc = setInterval(function func( ){
+ 
+        createField.getarray();
+        
         var res = snake.move();
         var add = res.added;
         var rm  = res.removed;
         
-        // if ( snake.body.length > 7  ) {
-        //   setInterval( func, 200 )
-        //   console.log(snake.body.length)
-        // };
-         
+        if ( add.x ===  -1 || add.x === createField.f.length  ) {
 
-          if ( add.x ===  -1 || add.x === f.length  ){
-              clearInterval(proc); 
-            alert('GAME OVER');
-             
-        };
-         if (  add.y ===  -1 || add.y === f.length  ) {
-             clearInterval(proc);
-             alert('GAME OVER');
-
-         };
-
-          if (  f[add.y][add.x].classList.contains('red')  ){
-            alert();
-             clearInterval(proc);
-
-            alert('GAME OVER');
-           
-           }; 
-        
-        f[add.y][add.x].classList.add('red');
-       
-         if ( rm ) {
-             f[rm.y][rm.x].classList.remove('red');
-        };
-        
-
-         if ( f[add.y][add.x].classList.contains('food') ) {
-            food(  );
-             snake.body.push({x:0,y:0}); 
+            clearInterval(proc); 
+            game_over();            
+            return;
             };
+
+        if (  add.y ===  -1 || add.y === createField.f.length  ) {
+
+            clearInterval(proc);
+            game_over();
+            return;
+        };
+
+        if (  createField.f[add.y][add.x].classList.contains('red') ) {
+            clearInterval(proc);
+            game_over();
+            return;
+        }; 
+        
+        createField.f[add.y][add.x].classList.add('red');
+       
+        if ( rm ) {
+             createField.f[rm.y][rm.x].classList.remove('red');
+        };
+        
+
+        if ( createField.f[add.y][add.x].classList.contains('food') ) {
+           
+            food( createField.f[add.y][add.x], createField.f,  snake.body );
+            
+           
+          
+        };
 
      
 
@@ -111,40 +178,18 @@ f1[s][s1].classList.add('food');
       
        
 }, 300);
-})();
+})( createField );
 
 
-function food(   ) {
+createField.field( createField.getul() );
 
-     f1[s][s1].classList.remove('food');
+(function(createField){
+    var f1 = createField.f;
+    var s = 5;
+    var s1 = 3;
+    f1[s][s1].classList.add('food');
 
-     s = Math.floor(Math.random() * f.length ); 
-     s1 = Math.floor(Math.random() * f.length );
-     console.log( s + ' '+ s1 )
+})(createField);
 
-     f1[s][s1].classList.add('food');
-    
-}
-
-var f = [];
-
-var ul = document.getElementById('field');
-
-for ( var x = 0; x < 31; x++ ) {
-    var li = document.createElement('li');
-    var string = [];
-    f.push(string);
-    ul.appendChild(li);
-    for ( var y = 0; y < 31; y++ ) {
-        var div = document.createElement('div');
-        string.push(div);
-        li.appendChild(div);
-        
-    }
-}
-
-console.log(f);
- var f1 = f;
-console.log(f1[1]);//////
 
 })();
